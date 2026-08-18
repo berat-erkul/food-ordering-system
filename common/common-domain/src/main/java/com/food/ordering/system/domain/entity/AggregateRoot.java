1fa1a7203a1f60e6e3911bdc24097d5621c8a399
beraterkul <beraterkul00@gmail.com>
2026-08-18 14:32:07 +0300

    feat: add DDD domain building blocks to common-domain module
    - Add BaseId<T>, Money, CustomerId, OrderId, ProductId, RestaurantId value objects
    - Add OrderStatus enum and DomainEvent marker interface

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.domain.entitiy;
+package com.food.ordering.system.domain.entity;
 
 public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
 
--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.domain.entitiy;
+package com.food.ordering.system.domain.entity;
 
 import java.util.Objects;
 
